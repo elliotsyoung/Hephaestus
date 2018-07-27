985929a9ff92a7e3ec2529a084bf303555a13e80
Justin <Justin@Justins-MacBook-Pro.local>
2018-07-26 18:03:42 -0700

    added other 4 macro buttons

--- src/components/QuickCommands.js
+++ src/components/QuickCommands.js
@@ -8,7 +8,7 @@ class myForm extends React.Component
     super(props);
 
   }
-  
+
   render()
   {
     return (
@@ -16,12 +16,10 @@ class myForm extends React.Component
       <div className="quick-wrapper">
 
             <button onClick={this.props.buttonOneClick} type="submit" className="">First Macro</button>
-
-            <button type="submit" className="">Second Macro</button>
-            <button type="submit" className="">Third Macro</button>
-            <button type="submit" className="">Fourth Macro</button>
-            <button type="submit" className="">Fifth Macro</button>
-
+            <button onClick={this.props.buttonTwoClick} type="submit" className="">Second Macro</button>
+            <button onClick={this.props.buttonThreeClick} type="submit" className="">Third Macro</button>
+            <button onClick={this.props.buttonFourClick} type="submit" className="">Fourth Macro</button>
+            <button onClick={this.props.buttonFiveClick} type="submit" className="">Fifth Macro</button>
             <br/>
             <br/>
             <button onClick={ this.props.toggleQuickCommandsListVisibility }>Toggle things</button>
